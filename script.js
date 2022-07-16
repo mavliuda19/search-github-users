150b86bf8001c699be8e3f99ef0a10529f99daa2
mavliuda19 <97443166+mavliuda19@users.noreply.github.com>
2022-07-16 15:50:51 +0600

    get users

---
+++ script.js
@@ -0,0 +1,45 @@
+const BASE_URL = 'https://api.github.com/search/users'
+
+const searchInput = document.getElementById('search-input')
+const searchButton = document.getElementById('button')
+const perPage = document.getElementById('count')
+const userList = document.getElementById('users')
+
+const renderUsers = ({ avatar_url, login, html_url }) => {
+	return `
+	<div class="user-container">
+	<div class="user-data">
+	<div class="avatar-wrapper">
+	<img class="avatar" src=${avatar_url}/>
+	<div>
+	<p>${login}</p>
+	<a href=${html_url}>link to github</a>
+	</div>
+	</div>
+	<div class="button-wrapper">
+	<button>star</button>
+	<button class="header-button">Show repositories</button>
+	</div>
+	</div>
+	</div>`
+}
+
+const renderUserList = (data) => {
+	let users = []
+	data.items.map((item) => {
+		users.push(renderUsers(item))
+	})
+	userList.innerHTML = users
+}
+
+const searchUser = async () => {
+	const inputValue = searchInput.value.split(' ').join('')
+	const countValue = count.value.split(' ').join('')
+	const response = await fetch(
+		`${BASE_URL}?q=${inputValue}&per_page=${countValue}`,
+	)
+	const data = await response.json()
+	renderUserList(data)
+}
+
+searchButton.addEventListener('click', searchUser)
