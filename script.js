067bbdc26f5e0a265332c6bb9ca9c4d5d82d8b8e
mavliuda19 <97443166+mavliuda19@users.noreply.github.com>
2022-07-17 12:05:23 +0600

    implement pagination

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ const perPage = document.getElementById('count')
 const userList = document.getElementById('users')
 const selectUser = document.getElementById('select')
 const selectedOrder = document.getElementById('select-order')
+const nextButton = document.getElementById('next-button')
+const prevButton = document.getElementById('prev-button')
 
 let selectedSortValue = 'favorities'
 let selectedOrderValue = 'desc'
+let currentPage = 1
+let inputValue = ''
+let paginationLimit = 4
+let pageCount
 
 const renderUsers = ({ avatar_url, login, html_url }) => {
 	return `
@@ -29,33 +35,59 @@ const renderUsers = ({ avatar_url, login, html_url }) => {
 	</div>`
 }
 
+let users = []
 const renderUserList = (data) => {
-	let users = []
+	users = []
 	data.items.map((item) => {
 		users.push(renderUsers(item))
 	})
-	userList.innerHTML = users
+	userList.innerHTML = users.join('')
 }
 
+const changeInputValue = (e) => {
+	inputValue = e.target.value.split(' ').join('')
+}
+const perPageInput = (e) => {
+	paginationLimit = e.target.value.split(' ').join('')
+}
 const sortSelectedValue = (event) => {
 	selectedSortValue = event.target.value
+	getData()
 }
 const orderSelectedValue = (event) => {
 	selectedOrderValue = event.target.value
+	getData()
 }
-
-const searchUser = async () => {
-	const inputValue = searchInput.value.split(' ').join('')
-	const countValue = count.value.split(' ').join('')
+// pagination
+const pagination = (data) => {
+	pageCount = Math.ceil(data.total_count / paginationLimit)
+}
+const prevPage = () => {
+	if (currentPage > 1) {
+		currentPage--
+		getData()
+	}
+}
+const nextPage = () => {
+	if (currentPage * paginationLimit < pageCount) {
+		currentPage++
+		getData()
+	}
+}
+// getdata
+const getData = async () => {
 	const response = await fetch(
-		`${BASE_URL}?q=${inputValue}&per_page=${countValue}&sort=${selectedSortValue}&order=${selectedOrderValue}`,
+		`${BASE_URL}?q=${inputValue}&per_page=${paginationLimit}&sort=${selectedSortValue}&order=${selectedOrderValue}&page=${currentPage}`,
 	)
 	const data = await response.json()
 	renderUserList(data)
+	pagination(data)
 }
 
 selectUser.addEventListener('click', sortSelectedValue)
 selectedOrder.addEventListener('click', orderSelectedValue)
-selectUser.addEventListener('click', searchUser)
-selectedOrder.addEventListener('click', searchUser)
-searchButton.addEventListener('click', searchUser)
+searchButton.addEventListener('click', getData)
+searchInput.addEventListener('change', changeInputValue)
+perPage.addEventListener('change', perPageInput)
+nextButton.addEventListener('click', nextPage)
+prevButton.addEventListener('click', prevPage)
